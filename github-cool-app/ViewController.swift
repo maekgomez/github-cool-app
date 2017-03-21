ad7277f4e3965abfcb45bde285c3fe6ff7331eeb
Michael Gomez <michaelgomez@protonmail.com>
2017-03-21 14:46:46 +0400

    Initial Commit

---
+++ github-cool-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  github-cool-app
+//
+//  Created by Michael Gomez on 3/21/17.
+//  Copyright © 2017 Michael Gomez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
